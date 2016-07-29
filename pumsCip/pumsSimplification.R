a4b0fa48b5b0c44942783f71d50e9f9a5cfeb035
bekahdevore <devore.rc@gmail.com>
2016-07-29 18:16:34 -0400

    ranking

--- pumsCip/pumsSimplification.R
+++ pumsCip/pumsSimplification.R
@@ -1,10 +1,32 @@
 library(stringr)
 
-maxRows <- by(pumsCipSoc, pumsCipSoc$PUMS, function(X) X[which.max(X$Median.Hourly.Earnings),])
-test <- merge(max)
-
 pumsCipSoc$Median.Hourly.Earnings <- str_replace_all(pumsCipSoc$Median.Hourly.Earnings, '\\$','')
-pumsCipSoc$Median.Hourly.Earnings <- as.numeric(pumsCipSoc$Median.Hourly.Earnings)
+pumsCipSoc$X2016.Jobs             <- str_replace_all(pumsCipSoc$X2016.Jobs, '<10','0')
+pumsCipSoc$X2016.Jobs             <- str_replace_all(pumsCipSoc$X2016.Jobs, ',','')
 
-df.agg <- aggregate(Median.Hourly.Earnings ~ PUMS, pumsCipSoc, max)
-pumsCipSoc <- merge(pumsCipSoc, df.agg)
+pumsCipSoc$Median.Hourly.Earnings <- as.numeric(pumsCipSoc$Median.Hourly.Earnings)
+pumsCipSoc$X2016.Jobs             <- as.numeric(as.character(pumsCipSoc$X2016.Jobs))
+
+#Ranking for Median Wage
+rankingMedian <- pumsCipSoc[order(pumsCipSoc$PUMS, -xtfrm(pumsCipSoc$Median.Hourly.Earnings)), ]
+rankingMedian$Median <- sequence(rle(r$PUMS)$lengths)
+
+#Ranking for Jobs
+rankingJobs <- pumsCipSoc[order(pumsCipSoc$PUMS -xtfrm(pumsCipSoc$X2016.Jobs)), ]
+rankingJobs$jobs <- sequence(rle(r$PUMS)$lengths)
+
+newData <- merge(rankingJobs, rankingMedian, by="CIP")
+newData <- newData %>%
+              mutate(cummulativeRank = (jobs+Median)/2)
+
+df.agg <- aggregate(cummulativeRank ~ PUMS.x, newData, max)
+newData <- merge(newData, df.agg)
+newData <- newData %>%
+                     select(PUMS.x, CIP)
+
+pumsNames <- read.csv("pumsDegree.csv")
+newData   <- merge(pumsNames, newData, by="PUMS.x")
+
+
+
+write.csv(newData, file = "pumsCipSocRanking01.csv")
