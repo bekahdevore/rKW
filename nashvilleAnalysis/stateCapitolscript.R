1576ccec02ce8ce4a0bec8012feda38f4f026173
bekahdevore <devore.rc@gmail.com>
2016-08-15 16:15:12 -0400

    state capitols script, new plot with ggplot and colors

--- nashvilleAnalysis/stateCapitolscript.R
+++ nashvilleAnalysis/stateCapitolscript.R
@@ -1,34 +1,37 @@
 #load packages
 library(dplyr)
+library(ggplot2)
 
 #Load data
-nashvilleData <- read.csv("nashvilleAnalysisData.csv")
+nashvilleData <- read.csv("stateCapitolData.csv")
+
 
 
 
 #Dummy variable
 nashvilleData <- nashvilleData %>%
-                     mutate(stateCapitol = ifelse(State.Capitol. == "No", 0,
-                                                  ifelse(State.Capitol. == "Yes", 1, 2)))
+                     mutate(stateCapitol = ifelse(State.Capital == "No", 0,
+                                               ifelse(State.Capital == "Yes", 1, 2)))
+
+#Change variable types
+nashvilleData$Annual.Median.Wage <- as.character(nashvilleData$Annual.Median.Wage)
+nashvilleData$Annual.Median.Wage <- as.numeric(gsub(",","",nashvilleData$Annual.Median.Wage))
 
 
-nashvilleData$A_MEDIAN <- as.character(nashvilleData$A_MEDIAN)
-nashvilleData$A_MEDIAN <- as.numeric(gsub(",","",nashvilleData$A_MEDIAN ))
-nashvilleData$A_MEDIAN <- as.numeric(nashvilleData$A_MEDIAN)
-
+#Visually cut off outliers
 abridgedMedian <-nashvilleData %>% 
-       filter(A_MEDIAN < 46000)
-
-
+       filter(Annual.Median.Wage < 46000)
 
 
 anovaTests <- function(dataInput){
                      noncapitols <- dataInput %>%
-                            filter(State.Capitol. == "No")
+                            filter(State.Capital == "No")
                      
                      capitols <- dataInput %>%
-                            filter(State.Capitol. == "Yes")
-
+                            filter(State.Capital == "Yes")
+                     
+                     #Create random samples of noncapitols to match the number of 
+                     #states with capitols
                      sample1 <- noncapitols[sample(1:nrow(noncapitols), 36,
                                                    replace=FALSE),]
                      sample2 <- noncapitols[sample(1:nrow(noncapitols), 36,
@@ -36,17 +39,38 @@ anovaTests <- function(dataInput){
                      sample3 <- noncapitols[sample(1:nrow(noncapitols), 36,
                                                    replace=FALSE),]
                      
+                     #Combine capitols with each sample to create test cases
                      test1 <- rbind(capitols, sample1)
                      test2 <- rbind(capitols, sample2)
                      test3 <- rbind(capitols, sample3)
                      
                      
-                     plot(A_MEDIAN ~ State.Capitol., data=test1)
-                     plot(A_MEDIAN ~ State.Capitol., data=test2)
-                     plot(A_MEDIAN ~ State.Capitol., data=test3)
+                     
+                     #Plot each test case
+                     plot(Annual.Median.Wage ~ State.Capital, data=test1)
+                     plot(Annual.Median.Wage ~ State.Capital, data=test2)
+                     plot(Annual.Median.Wage ~ State.Capital, data=test3)
+                     
+                     p <- ggplot(test1, aes(factor(State.Capital), Annual.Median.Wage))
+                     p + geom_boxplot(aes(fill=factor(State.Capital)))
+                     
+                     p <- ggplot(test2, aes(factor(State.Capital), Annual.Median.Wage))
+                     p + geom_boxplot(aes(fill=factor(State.Capital)))
+                     
+                     p <- ggplot(test3, aes(factor(State.Capital), Annual.Median.Wage))
+                     p + geom_boxplot(aes(fill=factor(State.Capital)))
+                     
 }
 
+#Change column names, more user friendly names
+
+
+
+#Run with data
 anovaTests(abridgedMedian)
+
+
+
 #ANOVA
 nashvilleAnova <- aov(nashvilleData$A_MEDIAN ~ nashvilleData$State.Capitol.)
 
@@ -65,4 +89,28 @@ influence(req)
 vcov(req)
 abline(req)
 coef(summary(req))["stateCapitol","Pr(>|t|)"]
+
+####TESTING##############
+
+noncapitols <- abridgedMedian %>%
+       filter(State.Capital == "No")
+
+capitols <- abridgedMedian %>%
+       filter(State.Capital == "Yes")
+
+sample1 <- noncapitols[sample(1:nrow(noncapitols), 36,
+                              replace=FALSE),]
+sample2 <- noncapitols[sample(1:nrow(noncapitols), 36,
+                              replace=FALSE),]
+sample3 <- noncapitols[sample(1:nrow(noncapitols), 36,
+                              replace=FALSE),]
+
+test1 <- rbind(capitols, sample1)
+test2 <- rbind(capitols, sample2)
+test3 <- rbind(capitols, sample3)
+
+
+boxplot(Annual.Median.Wage ~ State.Capital, data=test1)
+boxplot(Annual.Median.Wage ~ State.Capital, data=test2)
+boxplot(Annual.Median.Wage ~ State.Capital, data=test3)
 
