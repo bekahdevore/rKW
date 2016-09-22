28d60dd12e18888b1ac1cadd358bc02831215356
bekahdevore <devore.rc@gmail.com>
2016-09-22 12:29:02 -0400

    bls update script

--- blsUpdates/blsUpdateVisualizations.R
+++ blsUpdates/blsUpdateVisualizations.R
@@ -1,8 +1,17 @@
-
+#load packages
 library(stringr)
 
+#import data 
+laborForce         <- read.csv("laborForce.csv")
+unemploymentRate   <- read.csv("unemploymentRate.csv")
 
-laborForce       <- read.csv("laborForce.csv")
-unemploymentRate <- read.csv("unemploymentRate.csv")
+#Function to remove unecessary characters: (R), (D). and (P)
+replaceCharacters  <- function(dataName) {
+                            as.data.frame(lapply(dataName, function(x) {
+                            gsub("\\(R)|\\(D)|\\(P)", "", x)
+                            }))
+                     }
 
-
+#Remove characters from datasets
+laborForce       <- replaceCharacters(laborForce)
+unemploymentRate <- replaceCharacters(unemploymentRate)
