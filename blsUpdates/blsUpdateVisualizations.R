6de8c42537f26c50f703013606c16e1ee5fae8a2
bekahdevore <devore.rc@gmail.com>
2016-09-22 13:36:29 -0400

    bls updates

--- blsUpdates/blsUpdateVisualizations.R
+++ blsUpdates/blsUpdateVisualizations.R
@@ -1,9 +1,10 @@
 #load packages
 library(stringr)
+library(ggplot2)
 
 #import data 
-laborForce         <- read.csv("laborForce.csv")
-unemploymentRate   <- read.csv("unemploymentRate.csv")
+laborForceData         <- read.csv("laborForceJuly.csv")
+unemploymentRateData   <- read.csv("unemploymentRateJuly.csv")
 
 #Function to remove unecessary characters: (R), (D). and (P)
 replaceCharacters  <- function(dataName) {
@@ -13,5 +14,30 @@ replaceCharacters  <- function(dataName) {
                      }
 
 #Remove characters from datasets
-laborForce       <- replaceCharacters(laborForce)
-unemploymentRate <- replaceCharacters(unemploymentRate)
+laborForceData       <- replaceCharacters(laborForceData)
+unemploymentRateData <- replaceCharacters(unemploymentRateData)
+
+laborForceData$unemploymentRate       <- as.numeric(as.character(laborForceData$laborForce))
+unemploymentRateData$unemploymentRate <- as.numeric(as.character(unemploymentRateData$unemploymentRate)) 
+
+#plot on line graph
+ggplot(laborForce, aes(x=year, y=laborForce, group=area, color=area)) +
+       geom_line(size=1.2) +
+       geom_point(size=3)  +
+       xlab("Year")        +
+       ylab("laborForce")
+       
+ggplot(unemploymentRateData, aes(x=year, y=unemploymentRate, group=area, color=area)) +
+       geom_line(size=1.2)                              +
+       geom_point(size=3)                               +
+       xlab("Year")                                     +
+       ylab("Unemployment Rate")                        +
+       labs(title="Unemployment Rates, July 2006-2016") +
+       theme(plot.title = element_text(size = rel(2.5)), 
+              axis.title.y = element_text(size = rel(2)), 
+              axis.title.x = element_text(size = rel(2)), 
+              legend.position = c(.8, .8), 
+              legend.background = element_rect(color = "black"), 
+              legend.key = element_rect(color = "black")
+             )
+
