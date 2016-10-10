da7af1b3f478d17f63a92691c896cec266c2e220
bekahdevore <devore.rc@gmail.com>
2016-10-10 14:56:44 -0400

    bachelors plus occupations

--- bachelorsPlusOccupations/bachelorsPlusOccupations.R
+++ bachelorsPlusOccupations/bachelorsPlusOccupations.R
@@ -3,8 +3,8 @@ library(plotly)
 library(treemap)
 
 
-allOccupations       <- read.csv('allOccupationsSummary2.csv')
-businessOccupations  <- read.csv('businessBachelorsPlusSummaryLouisville.csv')
+allOccupations       <- read.csv('last90daysAll.csv')
+businessOccupations  <- read.csv('last90daysBusiness.csv')
 majorSocCodeNames    <- read.csv('socMajorOccupationGroupsBLS_2010.csv')
 
 #majorSocCodeNames$SOC <- as.numeric(as.character(majorSocCodeNames$SOC))
@@ -14,40 +14,46 @@ all      <- right_join(allOccupations, majorSocCodeNames, by = 'SOC')
 
 
 ################################## FUNCTIONS ###########################################
-selectFilter <- function(dataName) {dataName %>% 
-                     select(SOC, Occupation, Number.of.Job.Postings)}
-
-filterPercent <- function(dataName) {dataName %>%
-                     filter(per >= .01)}
-
-pieChart      <- function(dataName, chartTitle) {plot_ly(dataName, labels = Occupation, values = n, type = "pie") %>%
-                     layout(title = chartTitle)}
-
-treeMapMaker  <- function(dataName) {treemap(dataName, index = c('Occupation'), vSize = 'n', 
-                     fontsize.labels = 12,
-                     border.col = 0,
-                     title = "",
-                     fontface.labels = 1,
-                     fontfamily.labels = "sans")}
-
-
-percent       <- function(data){
-                     data <-  data %>% mutate(per=n/sum(n))
-                     print(data)
-}
-
-removeCommas <- function(dataName) {as.data.frame(lapply(dataName, function(x) {
-                     gsub(",", "", x)}))
-}
-
-countPercent <- function(dataName) {count(dataName, wt = Number.of.Job.Postings)
-                     x <- count(dataName, Occupation, wt= Number.of.Job.Postings, sort = TRUE) #weight by Job Postings
-                     percent(x)}
+       selectFilter <- function(dataName) {dataName %>% 
+                            select(SOC, Occupation, Number.of.Job.Postings)}
+       
+       removeCommas <- function(dataName) {as.data.frame(lapply(dataName, function(x) {
+                            gsub(",", "", x)}))
+       }
 
 cleanData <- function(dataName) {
                      selectFilter(dataName)
                      removeCommas(dataName)
 }
+
+       percent       <- function(data){
+              data <-  data %>% mutate(per=n/sum(n))
+              print(data)
+       }
+
+countPercent <- function(dataName) {count(dataName, wt = Number.of.Job.Postings)
+       x <- count(dataName, Occupation, wt= Number.of.Job.Postings, sort = TRUE) #weight by Job Postings
+       percent(x)}
+
+filterPercent <- function(dataName) {dataName %>%
+              filter(per >= .01)}
+
+
+pieChart      <- function(dataName, chartTitle) {
+                     dataName <- filterPercent(dataName)
+                     
+                     plot_ly(dataName, labels = Occupation, values = n, type = "pie") %>%
+                     layout(title = chartTitle)}
+
+treeMapMaker  <- function(dataName) {
+                     dataName <- filterPercent(dataName)              
+       
+                     treemap(dataName, index = c('Occupation'), vSize = 'n', 
+                     fontsize.labels = 18,
+                     border.col = 0,
+                     title = "",
+                     fontface.labels = 1,
+                     fontfamily.labels = "sans")}
 #############################################################################################################################
 
 
@@ -60,13 +66,17 @@ business$Number.of.Job.Postings <- as.numeric(as.character(business$Number.of.Jo
 all      <- countPercent(all)
 business <- countPercent(business)
 
+
+######################################### OUTPUT ####################################################
 write.csv(all, file = 'all.csv')
 write.csv(business, file = 'business.csv')
 
 
-pieChart(pieAll, "All Occupations, Bachelor's or higher")
-pieChart(pieBusiness, 'Business')
+pieChart(all, "All Occupations, Bachelor's or higher")
+pieChart(business, 'Business')
 
 
+treeMapMaker(all)
+treeMapMaker(business)
 
 
