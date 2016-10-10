fe179ee98ea5a770b2e38153e630bf232fae4b3d
bekahdevore <devore.rc@gmail.com>
2016-10-10 18:23:33 -0400

    bachelors plus occupations

--- bachelorsPlusOccupations/bachelorsPlusOccupations.R
+++ bachelorsPlusOccupations/bachelorsPlusOccupations.R
@@ -21,8 +21,8 @@ allThreeNinety      <- read.csv('all3plusLast90days.csv')
        }
 
 cleanData <- function(dataName) {
-                     selectFilter(dataName)
-                     removeCommas(dataName)
+                     dataName <- selectFilter(dataName)
+                     dataName <- removeCommas(dataName)
 }
 
        percent       <- function(data){
@@ -34,8 +34,9 @@ countPercent <- function(dataName) {count(dataName, wt = Number.of.Job.Postings)
        x <- count(dataName, Occupation, wt= Number.of.Job.Postings, sort = TRUE) #weight by Job Postings
        percent(x)}
 
-filterPercent <- function(dataName) {dataName %>%
-              filter(per >= .01)}
+
+       filterPercent <- function(dataName) {dataName %>%
+                     filter(per >= .01)}
 
 
 pieChart      <- function(dataName, chartTitle) {
@@ -54,8 +55,37 @@ treeMapMaker  <- function(dataName) {
                      title = "",
                      fontface.labels = 1,
                      fontfamily.labels = "sans")}
-#############################################################################################################################
 
+#############################################################################################################################
+########################################## 3 to 5 year Stacked Bar Graph Data Manipulation ####################################
+business            <- right_join(businessOccupations, majorSocCodeNames, by = 'SOC')
+all                 <- right_join(allOccupations, majorSocCodeNames, by = 'SOC') 
+businessThreeNinety <- right_join(businessThreeNinety, majorSocCodeNames, by = 'SOC')
+allThreeNinety      <- right_join(allThreeNinety, majorSocCodeNames, by = 'SOC')
+
+
+businessThreeNinety <- cleanData(businessThreeNinety)
+allThreeNinety      <- cleanData(allThreeNinety)
+business            <- cleanData(business)
+all                 <- cleanData(all)
+
+
+all$Number.of.Job.Postings      <- as.numeric(as.character(all$Number.of.Job.Postings))
+business$Number.of.Job.Postings <- as.numeric(as.character(business$Number.of.Job.Postings))
+allThreeNinety$Number.of.Job.Postings      <- as.numeric(as.character(allThreeNinety$Number.of.Job.Postings))
+businessThreeNinety$Number.of.Job.Postings <- as.numeric(as.character(businessThreeNinety$Number.of.Job.Postings))
+
+
+allThreeNinety      <- countPercent(allThreeNinety)
+businessThreeNinety <- countPercent(businessThreeNinety)
+all                 <- countPercent(all)
+business            <- countPercent(business)
+
+business <- full_join(business, businessThreeNinety, by = 'Occupation')
+all      <- full_join(all, allThreeNinety, by = 'Occupation')
+######################################### ORIGNIAL OUTPUT ####################################################
+write.csv(all, file = 'all.csv')
+write.csv(business, file = 'business.csv')
 
 all      <- cleanData(all)
 business <- cleanData(business)
@@ -67,41 +97,20 @@ all      <- countPercent(all)
 business <- countPercent(business)
 
 all$numberLabel <- format(all$n,
-                     big.mark   = ",", 
-                     scientific = FALSE)
+                          big.mark   = ",", 
+                          scientific = FALSE)
 
 all$label       <- paste(all$Occupation, 
-                     all$numberLabel,
-                     sep = "\n")
+                         all$numberLabel,
+                         sep = "\n")
 
 business$numberLabel <- format(business$n,
-                            big.mark   = ",", 
-                            scientific = FALSE)
+                               big.mark   = ",", 
+                               scientific = FALSE)
 
 business$label       <- paste(business$Occupation, 
-                            business$numberLabel,
-                            sep = "\n")
-
-
-########################################## 3 to 5 year Stacked Bar Graph Data Manipulation ####################################
-business <- right_join(businessOccupations, majorSocCodeNames, by = 'SOC')
-all      <- right_join(allOccupations, majorSocCodeNames, by = 'SOC') 
-
-businessThreeNinety <- right_join(businessThreeNinety, majorSocCodeNames, by = 'SOC')
-allThreeNinety      <- right_join(allThreeNinety, majorSocCodeNames, by = 'SOC')
-
-
-businessThreeNinety <- selectFilter(businessThreeNinety)
-allThreeNinety      <- selectFilter(allThreeNinety)
-
-
-
-
-
-
-######################################### OUTPUT ####################################################
-write.csv(all, file = 'all.csv')
-write.csv(business, file = 'business.csv')
+                              business$numberLabel,
+                              sep = "\n")
 
 
 pieChart(all, "All Occupations, Bachelor's or higher")
