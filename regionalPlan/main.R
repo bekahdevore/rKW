463dbedd265bed617b3d9b32f7ed083a60caae7e
bekahdevore <devore.rc@gmail.com>
2017-05-01 17:07:28 -0400

    updated regional plan project

--- regionalPlan/main.R
+++ regionalPlan/main.R
@@ -1,20 +1,50 @@
 library(RCurl)
 library(dplyr)
-
+library(stringr)
 #Connect to data url's
 qcewAllIndustriesDataConnection <- getURL("https://docs.google.com/spreadsheets/d/1D4d6pTndk6CffoEnGr6DZn95Q7PVbQyCRHwEVLp2iGg/pub?gid=276897935&single=true&output=csv")
-manufacturingDataConnection <- getURL("https://docs.google.com/spreadsheets/d/1D4d6pTndk6CffoEnGr6DZn95Q7PVbQyCRHwEVLp2iGg/pub?gid=411223081&single=true&output=csv")
+specificIndustriesDataConnection <- getURL("https://docs.google.com/spreadsheets/d/1D4d6pTndk6CffoEnGr6DZn95Q7PVbQyCRHwEVLp2iGg/pub?gid=1004580518&single=true&output=csv")
 countiesDataConnection <- getURL("https://docs.google.com/spreadsheets/d/1D4d6pTndk6CffoEnGr6DZn95Q7PVbQyCRHwEVLp2iGg/pub?gid=468101338&single=true&output=csv")
 
 #Read data into R from URL's 
 qcewAllIndustriesData <- read.csv(textConnection(qcewAllIndustriesDataConnection)) #Make new column with annualized wage, filter to only columns that are needed
-manufacturingData <- read.csv(textConnection(manufacturingDataConnection))
+specificIndustriesData <- read.csv(textConnection(specificIndustriesDataConnection))
 counties <- read.csv(textConnection(countiesDataConnection))
+
+
 
 #Remove data that is no longer needed
 rm(qcewAllIndustriesDataConnection, manufacturingDataConnection, countiesDataConnection)
 
 # FUNCTIONS
+removeCommas <- function(data) {
+  data <- as.data.frame(lapply(data,function(x) {gsub(",", "", x)
+                        }))
+}
+
+removeDollarSigns <- function(data) {
+  data <- as.data.frame(lapply(data,function(x) {gsub("\\$", "", x)
+                        }))
+}
+
+
+
+
+changeDataType <- function(data) {
+  names <- c("July.Employment", "August.Employment", "September.Employment", "Average.Weekly.Wage")
+  data[,names] <- lapply(data[,names] , as.character)
+  data[,names] <- lapply(data[,names] , as.numeric)
+  data
+}
+
+
+annalizeAndSelect <- function(data) {
+  this <- data %>% mutate(annualizedWage = (Average.Weekly.Wage * 52)) %>% 
+    mutate(averageEmployment = ((July.Employment + August.Employment + September.Employment)/3))
+   output <- this %>% select(County, annualizedWage, averageEmployment)
+   output
+}
+
 fortyCountyFilter <- function(data) {
   data %>% filter(County %in% counties$allCounties)
 }
@@ -31,6 +61,14 @@ lincolnTrailFilter <- function(data) {
   data %>% filter(County %in% counties$lincolnTrail)
 }
 
+manipulateData <- function(insertData, insertFilter) {annalizeAndSelect(changeDataType(removeDollarSigns(removeCommas(insertFilter(insertData)))))}
 
 
- <- fortyCountyFilter(qcewAllIndustriesData)
+# Run Data
+fortyCountiesData <- manipulateData(qcewAllIndustriesData, fortyCountyFilter)
+kentuckianaWorksData <- manipulateData(qcewAllIndustriesData, kentuckianaWorksFilter)
+bluegrassData <- manipulateData(qcewAllIndustriesData, bluegrassFilter)
+lincolnTrailData <- manipulateData(qcewAllIndustriesData, lincolnTrailFilter)
+
+
+
