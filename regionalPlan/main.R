edb0a9cbaa0dd9dd98cd135d2d91a97543bc5e18
bekahdevore <devore.rc@gmail.com>
2017-05-02 10:39:30 -0400

    updated regional plan project

--- regionalPlan/main.R
+++ regionalPlan/main.R
@@ -1,6 +1,7 @@
 library(RCurl)
 library(dplyr)
 library(stringr)
+
 #Connect to data url's
 qcewAllIndustriesDataConnection <- getURL("https://docs.google.com/spreadsheets/d/1D4d6pTndk6CffoEnGr6DZn95Q7PVbQyCRHwEVLp2iGg/pub?gid=276897935&single=true&output=csv")
 specificIndustriesDataConnection <- getURL("https://docs.google.com/spreadsheets/d/1D4d6pTndk6CffoEnGr6DZn95Q7PVbQyCRHwEVLp2iGg/pub?gid=1004580518&single=true&output=csv")
@@ -10,7 +11,6 @@ countiesDataConnection <- getURL("https://docs.google.com/spreadsheets/d/1D4d6pT
 qcewAllIndustriesData <- read.csv(textConnection(qcewAllIndustriesDataConnection)) #Make new column with annualized wage, filter to only columns that are needed
 specificIndustriesData <- read.csv(textConnection(specificIndustriesDataConnection))
 counties <- read.csv(textConnection(countiesDataConnection))
-
 
 
 #Remove data that is no longer needed
@@ -38,12 +38,23 @@ changeDataType <- function(data) {
 }
 
 
-annalizeAndSelect <- function(data) {
+annualizeAndSelect <- function(data) {
   this <- data %>% mutate(annualizedWage = (Average.Weekly.Wage * 52)) %>% 
     mutate(averageEmployment = ((July.Employment + August.Employment + September.Employment)/3))
-   output <- this %>% select(County, annualizedWage, averageEmployment)
-   output
+  this <- this %>% mutate(totalEmployment = sum(averageEmployment)) %>% mutate(averageWage = mean(annualizedWage))
+  output <- this %>% select(County, annualizedWage, averageEmployment, totalEmployment, averageWage)
+  output
 }
+
+annualizeAndSelectIndustry <- function(data, insertIndustry) {
+  this <- data %>% mutate(annualizedWage = (Average.Weekly.Wage * 52)) %>% 
+    mutate(averageEmployment = ((July.Employment + August.Employment + September.Employment)/3)) %>%
+    filter(industry == insertIndustry) 
+  this <- this %>% mutate(totalEmployment = sum(averageEmployment)) %>% mutate(averageWage = mean(annualizedWage))
+  output <- this %>% select(County, annualizedWage, averageEmployment, industry, totalEmployment, averageWage)
+  output
+}
+
 
 fortyCountyFilter <- function(data) {
   data %>% filter(County %in% counties$allCounties)
@@ -61,14 +72,36 @@ lincolnTrailFilter <- function(data) {
   data %>% filter(County %in% counties$lincolnTrail)
 }
 
-manipulateData <- function(insertData, insertFilter) {annalizeAndSelect(changeDataType(removeDollarSigns(removeCommas(insertFilter(insertData)))))}
+manipulateData <- function(insertData, insertFilter) {annualizeAndSelect(changeDataType(removeDollarSigns(removeCommas(insertFilter(insertData)))))}
+
+manipulateIndustryData <- function(insertData, insertFilter, insertIndustry) {
+                          annualizeAndSelectIndustry(changeDataType(removeDollarSigns(removeCommas(insertFilter(insertData)))), insertIndustry)
+}
 
 
-# Run Data
+# EACH AREA 40 COUNTIES DATA
 fortyCountiesData <- manipulateData(qcewAllIndustriesData, fortyCountyFilter)
 kentuckianaWorksData <- manipulateData(qcewAllIndustriesData, kentuckianaWorksFilter)
+northernKentuckyData <- manipulateData(qcewAllIndustriesData, northernKentuckyFilter)
 bluegrassData <- manipulateData(qcewAllIndustriesData, bluegrassFilter)
 lincolnTrailData <- manipulateData(qcewAllIndustriesData, lincolnTrailFilter)
+
+## FORTY COUNTIES BY INDUSTRY
+manufacturing <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "manufacturing")
+construction <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "construction")
+healthcare <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "healthcare")
+logistics <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "logistics")
+finance <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "finance")
+retail <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "retail")
+food <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "food")
+hospitality <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "hospitality")
+agriculture <- manipulateIndustryData(specificIndustriesData, fortyCountyFilter, "agriculture")
+
+## KENTUCKIANAWORKS DATA
+## BLUEGRASS DATA
+## NOTHERN KENTUCKY DATA
+## LINCOLN TRAIL DATA
+
 
 
 
