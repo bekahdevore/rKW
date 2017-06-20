6980d3d20c063fb8762d528a59acefe78f9472a2
bekahdevore <devore.rc@gmail.com>
2017-06-20 16:58:12 -0400

    MEWs one-pager, added oes data api connection

---
+++ mewOnePagerAPIs/getData.R
@@ -0,0 +1,48 @@
+library(dplyr)
+
+
+#### BLS OES MEDIAN WAGE DATA ####
+# list of all datasets 
+# https://download.bls.gov/pub/time.series/overview.txt
+
+import.from.bls <- function(web.address) {
+  
+  # Import data
+  temp <- tempfile()
+  download.file(web.address, temp)
+  data <- read.table(temp,
+                     header=FALSE,
+                     sep="\t",
+                     skip=1,
+                     stringsAsFactors=FALSE,
+                     strip.white=TRUE,
+                     quote = NULL)
+  
+  # Add column headers
+  topline <- readLines(web.address)
+  topline <- topline[1] # Select column headers
+  topline <- as.list(strsplit(x = topline, split = "\t")[[1]]) # Split the string into a list
+  colnames(data) <- topline
+  
+  unlink(temp)
+  
+  # Drop extra, unused column
+  data <- data[,1:(ncol(data) - 1)]
+  
+  # This command plucks the text that appears after the pattern below,
+  # and uses it to name the file.
+  filename <- gsub(pattern = "https://download.bls.gov/pub/time.series/oe/oe.","", x = web.address, ignore.case=T)
+  
+  # save the file to the global environment
+  assign(filename, data, envir = .GlobalEnv) 
+}
+
+import.from.bls("https://download.bls.gov/pub/time.series/oe/oe.series")
+import.from.bls("https://download.bls.gov/pub/time.series/oe/oe.data.0.Current")
+import.from.bls("https://download.bls.gov/pub/time.series/oe/oe.area")
+
+
+allData <- left_join(data.0.Current, series, by = "series_id")
+allData <- left_join(allData, area, by = "area_code")
+
+save(allData, file = "allData.RData")
---
+++ mewOnePagerAPIs/oes.R
@@ -0,0 +1,14 @@
+library(RCurl)
+library(dplyr)
+
+areaCodeConnection <- getURL("https://docs.google.com/spreadsheets/d/1MIgcX_LQBF2J2pzevXRPJzy9UKjaVls9vNda3Pgay3Q/pub?gid=0&single=true&output=csv")
+peerAreaCodes <- read.csv(textConnection(areaCodeConnection))
+ 
+load("allData.RData")
+
+medianWage <- allData %>% 
+  filter((area_code %in% peerAreaCodes$area_code & datatype_code == 13 & occupation_code == 0)) %>% 
+  select(20, 4, 13)
+
+medianWage$rank <- rank(desc(medianWage$value))
+
